8d57cc3991950e56aee43895e1d9d2798dc22ff5
Kevin Chang <Kevin.chang7677@gmail.com>
2025-03-06 23:39:06 -0800

    optimized!

--- WordGame.py
+++ WordGame.py
@@ -3,21 +3,22 @@
 import spacy
 import random
 import wordfreq
+import numpy as np
 from numpy.linalg import norm
 
 def main():
     
     iterations = int(input("How many rounds would you like to play? \n"))
     
+    print("processing words...")
     word_game = WordGame()
     points = 0
-    
-    #TODO: maybe only give people 5 guesses
-    
+        
     for i in range(iterations):
         
         print(f" \n Onto round {i+1}! \n")
-        outcome = word_game.simple_addition(0.52)
+        
+        outcome = word_game.simple_addition(0.55, 5)
         
         if outcome:
             points += 1
@@ -36,15 +37,25 @@ A little scuffed but I won't check if the operation makes sense.
 As for other operations, if they are too complex it also wouldn't make sense. Adding 3 words makes more
 sense than subtracting. But it would still work. In fact I should make everything one method, have the 
 number of inputs and specific operations as the parameter.
+
+Or I could preload 10 and see how fast people are able to complete them. The issue is that sometimes
+people will get an odd combination that will seem unfair. However this reduces the incentive to just
+give up on harder combinations in favor for super easy combinations, furthermore it will add a tangible
+penalty to guessing instead of the I can just move on mentality.
+
+However the feeling of time running out is also a really good motivator!
+What makes Wordle better however is the shared sense of struggle as everyone has the same word
+
+ISSUE: No way of showing "correct answer" unless maybe iterating through freq words until there is a 
+maximum similarity
 '''
 
 '''
-TODO: other operations for words, penalization for guessing, timer
-TODO: for actual website new word combination is too slow. Optimize + preload all of them so they don't
-    take up the user's timer. Preloading is possible if number of rounds is fixed. But it isn't, number of 
-    rounds is dependent on time. Could preload like 10 and have threads run in the background while the
-    user is playing.
+TODO: other operations for words, timer
+TODO: Increase accuracy! maybe use a larger model like gensim. Problems + annoying = nuisance being incorrect
+    will definitely make the experience bothersome.
 TODO: have website built before tuesday?
+TODO: one of the problems is that the solution is always one of the inputs
 '''
 
 class WordGame:
@@ -54,6 +65,8 @@ class WordGame:
         #TODO: can en_core_web_lg be installed on codepad? 
         self.nlp_model = spacy.load("en_core_web_lg")
         self.meaningful_words = self.load_meaningful_words(5000)
+        self.word_vectors = np.array([self.nlp_model(word)[0].vector for word in self.meaningful_words])
+        self.vector_norms = np.linalg.norm(self.word_vectors, axis=1)
         
         #print(f"finished processing {len(self.meaningful_words)}")
 
@@ -61,7 +74,9 @@ class WordGame:
         #self.word_vectors = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin", binary=True)
 
     
-    def simple_addition (self, threshold):
+    #TODO: a major issue is that if you just type a synonym of one of the two inputs, it will be correct
+    #TODO: can be solved if you check to make sure the similarity with the input vectors themselves isn't above a certain threshold
+    def simple_addition (self, threshold, num_guesses):
                         
         rand1 = int(random.random()*len(self.meaningful_words)-1)
         
@@ -74,22 +89,17 @@ class WordGame:
         doc1 = self.nlp_model(word1)
         word1_vector = doc1[0].vector
 
-        #making sure the words added together are somewhat similar
+
+        #making sure the words added together are somewhat similar   
         
-        close_words = []
+        dot_products = np.dot(self.word_vectors, word1_vector)
+        norms = self.vector_norms * np.linalg.norm(word1_vector)
+        cosine_similarities = dot_products/norms
         
-        for word in self.meaningful_words:
-            
-            temp_doc = self.nlp_model(word)
-            temp_vector = temp_doc[0].vector
-            
-            if self.cosine_similarity(word1_vector, temp_vector) > 0.37:
-                
-                close_words.append(word)
+        #(cosine_similarities > 0.37) creates own boolean mask, likewise for (cosine_similarities < 0.78)
+        close_words = [self.meaningful_words[i] for i in np.where((cosine_similarities > 0.37) & (cosine_similarities < 0.85))[0]]
         
-        #print(f"close words length: {len(close_words)} \n")
-        #potential issue is that close_words could be empty, but I'm betting that's not going to happen
-        
+        print(f"close words: {len(close_words)}")
         
         while True:
             rand2 = int(random.random()*len(close_words)-1)
@@ -108,14 +118,22 @@ class WordGame:
         similarity = 0
         
         
+        
         while True:
             
             guess = input(f"{word1} + {word2} = ? \n")  
 
             if guess == "I give up":
                 
+                print("round terminated \n")
+                print("Correct answer was: " + str(self.find_answer(sum_vector)) + "\n")
                 return False
-                #break
+            
+            if num_guesses == 0:
+                
+                print("Correct answer was: " + str(self.find_answer(sum_vector)) + "\n")
+                print(" Out of guesses! \n")
+                return False
             
             doc3 = self.nlp_model(guess)
             
@@ -136,12 +154,14 @@ class WordGame:
                 return True
                 #break
             
+            num_guesses -= 1
+            
             print("Incorrect, try again! \n")
             
             
 #TODO: Programming club challenge! Create a method for subtracting words!
 
-            
+#TODO: save this as a file so I can load these on directly in the future           
     def load_meaningful_words(self, common_count):
                 
         common_words = wordfreq.top_n_list("en", common_count)
@@ -158,6 +178,17 @@ class WordGame:
                             ]
         
         return meaningful_words    
+
+
+    #TODO: cannot be the cosine product of the word itself
+    def find_answer(self, result_vector):      
+                
+        dot_products = np.dot(self.word_vectors, result_vector)
+        norms = self.vector_norms * np.linalg.norm(result_vector)
+        cosine_similarities = dot_products/norms
+        
+        return self.meaningful_words[np.argmax(cosine_similarities)]
+
 
 
 
